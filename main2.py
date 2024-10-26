c855a30633b8d1f732a10cf3a3575f6deb189375
Matheus Rodrigues <140926249+matheusrodrrgues@users.noreply.github.com>
2024-10-26 17:06:25 -0300

    Update main2.py
    Versão beta final

--- main2.py
+++ main2.py
@@ -1,3 +1,16 @@
+# /*******************************************************************************
+# Autor: Matheus Silva Rodrigues
+# Componente Curricular: MI Algoritmos
+# Concluido em: 29/10/2024
+# Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+# trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+# apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+# de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+# do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+# SO utilizado: Windows 10
+# Bibliotecas utilizadas: CURSES, RANDOM e TIME
+# ******************************************************************************************/
+
 import curses
 import random
 import time
@@ -24,9 +37,9 @@ letras = [I, T, O, S, Z, J, L, BMB]
 
 def print_tabtetris(stdscr, pontos):
     stdscr.clear()
-    stdscr.addstr("Pontuação: {}\n".format(pontos))
     for linha in tabtetris:
         stdscr.addstr("".join(linha) + "\n")
+    stdscr.addstr("Pontuação: {}\n".format(pontos))
     stdscr.refresh()
 
 def colocar_letra(letra, linha, coluna):
@@ -76,33 +89,39 @@ def jogartetris(stdscr):
     letra_atual = random.choice(letras)
     linha_atual = 0
     coluna_atual = 3
+    tempo_descida = time.time()  # Controle de tempo para descida automática
     curses.curs_set(0)
-    stdscr.nodelay(1)
+    stdscr.nodelay(True)
 
     while True:
         print_tabtetris(stdscr, pontos)
-        time.sleep(0.3)
 
         # Limpar a posição anterior
         limpar_letra(letra_atual, linha_atual, coluna_atual)
 
-        # Movimento para baixo
-        linha_atual += 1
+        # Verificar o tempo para descer automaticamente a peça
+        if time.time() - tempo_descida > 0.5:
+            linha_atual += 1
+            tempo_descida = time.time()  # Resetar o temporizador de descida
+
+        # Checar colisão após descer
         if verificar_colisao(letra_atual, linha_atual, coluna_atual):
-            linha_atual -= 1
+            linha_atual -= 1  # Corrigir posição
             colocar_letra(letra_atual, linha_atual, coluna_atual)
             linhas_removidas = remover_linhas_completas()
             pontos += atualizar_pontuacao(linhas_removidas)
-            
-            # Checar se é Game Over
+
+            # Checar se é Game Over após colocar a letra
             letra_atual = random.choice(letras)
             linha_atual = 0
             coluna_atual = 3
-            
-            if game_over(letra_atual, linha_atual, coluna_atual):
+
+            # Verifica colisão com a nova letra
+            if verificar_colisao(letra_atual, linha_atual, coluna_atual):
                 stdscr.addstr(22, 0, "GAME OVER")
                 stdscr.refresh()
-                time.sleep(2)
+                stdscr.nodelay(False)
+                stdscr.getch()
                 break
 
         # Controle do jogador
@@ -116,7 +135,26 @@ def jogartetris(stdscr):
             if not verificar_colisao(nova_letra, linha_atual, coluna_atual):
                 letra_atual = nova_letra
 
+        # Colocar a peça no tabuleiro
         colocar_letra(letra_atual, linha_atual, coluna_atual)
 
+        # Pequeno delay para o loop, para garantir que o programa não consuma CPU excessivamente
+        curses.napms(50)
+
+def menu(stdscr):
+    while True:
+        stdscr.clear()
+        stdscr.addstr("Bem-vindo ao Tetris\n")
+        stdscr.addstr("Pressione 1 para iniciar o jogo\n")
+        stdscr.addstr("Pressione 0 para sair\n")
+        stdscr.refresh()
+        
+        key = stdscr.getch()
+        if key == ord('1'):
+            jogartetris(stdscr)
+        elif key == ord('0'):
+            print("Programa encerrado. Até logo!")
+            break
+
 if __name__ == "__main__":
-    curses.wrapper(jogartetris)
+    curses.wrapper(menu)
